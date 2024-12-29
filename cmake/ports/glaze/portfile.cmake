ccce214102181a27938917dc1ed67ee35abcc495
powerof3 <powerof3@users.noreply.github.com>
2024-12-29 06:05:44 +0530

    Disable AVX2 instructions

---
+++ cmake/ports/glaze/portfile.cmake
@@ -0,0 +1,26 @@
+if(VCPKG_TARGET_IS_LINUX)
+    message("Warning: `glaze` requires Clang15+ or GCC 12+ on Linux")
+endif()
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO stephenberry/glaze
+    REF "v${VERSION}"
+    SHA512 993a6a930c7ed5b67c47fee0380ff71404db7ac9e220c8f54ee5d818ff0fd0f9ca30aabf05cc6c6b74d6fadbc481f2fe7eb85493b9112c3ef9c28374fbbb686d
+    HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -Dglaze_DEVELOPER_MODE=OFF
+        -Dglaze_BUILD_EXAMPLES=OFF
+		-Dglaze_ENABLE_AVX2=OFF
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
