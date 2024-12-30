65d3ccaf5f5491f362de17bd0a6cf233b26224d8
FlayaN <hannes.feldt@gmail.com>
2024-12-30 23:00:15 +0100

    fix(VR): fix vr frustum culling

--- src/Hooks/Misc.cpp
+++ src/Hooks/Misc.cpp
@@ -74,6 +74,25 @@ namespace Hooks::Misc
 		}
 	};
 
+#ifdef SKYRIMVR
+	struct Patch_NiFrustrumPlanes__Set
+	{
+		static void Install()
+		{
+			const REL::Relocation NiFrustrumPlanes__Set{ REL::Offset(0xCB2760) };
+
+			// mov rdx, [rdx+180h] (48 8B 92 80 01 00 00)
+			//                         v  v  v
+			// add rdx, 1CCh       (48 81 C2 CC 01 00 00)
+
+			REL::safe_fill(NiFrustrumPlanes__Set.address() + 0x5, 0x81, 1);
+			REL::safe_fill(NiFrustrumPlanes__Set.address() + 0x6, 0xC2, 1);
+			REL::safe_fill(NiFrustrumPlanes__Set.address() + 0x7, 0xCC, 1);
+			logger::info("Patched NiFrustrumPlanes::Set");
+		}
+	};
+#endif
+
 	void Install()
 	{
 		if (Settings::GetSingleton()->ShouldDisableLights()) {
@@ -82,5 +101,8 @@ namespace Hooks::Misc
 
 		FreeCamera_Begin::Install();
 		FreeCamera_End::Install();
+#ifdef SKYRIMVR
+		Patch_NiFrustrumPlanes__Set::Install();
+#endif
 	}
 }
